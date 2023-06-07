e3d4527317275195637e4c18f175d1cabec9c453
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-06-07 21:14:37 +0800

    Create run.sh

---
+++ Final Project: Timing-Aware Fill Insertion/submit/verify/run.sh
@@ -0,0 +1,5 @@
+./verify ./input/1.txt ./output/1.txt > .verify/evaluator1.log
+./verify ./input/2.txt ./output/2.txt > .verify/evaluator2.log
+./verify ./input/3.txt ./output/3.txt > .verify/evaluator3.log
+./verify ./input/4.txt ./output/4.txt > .verify/evaluator4.log
+./verify ./input/5.txt ./output/5.txt > .verify/evaluator5.log
