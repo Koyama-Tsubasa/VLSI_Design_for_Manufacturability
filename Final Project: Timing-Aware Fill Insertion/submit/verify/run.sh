8b89efa9e84c8cdc600f86fcc9ed93e80c20dd88
K. Tsubasa <48341641+Koyama-Tsubasa@users.noreply.github.com>
2023-06-07 21:16:55 +0800

    Update run.sh

--- Final Project: Timing-Aware Fill Insertion/submit/verify/run.sh
+++ Final Project: Timing-Aware Fill Insertion/submit/verify/run.sh
@@ -1,5 +1,5 @@
-./verify ./input/1.txt ./output/1.txt > .verify/evaluator1.log
-./verify ./input/2.txt ./output/2.txt > .verify/evaluator2.log
-./verify ./input/3.txt ./output/3.txt > .verify/evaluator3.log
-./verify ./input/4.txt ./output/4.txt > .verify/evaluator4.log
-./verify ./input/5.txt ./output/5.txt > .verify/evaluator5.log
+../verify ../input/1.txt ../output/1.txt > evaluator1.log
+../verify ../input/2.txt ../output/2.txt > evaluator2.log
+../verify ../input/3.txt ../output/3.txt > evaluator3.log
+../verify ../input/4.txt ../output/4.txt > evaluator4.log
+../verify ../input/5.txt ../output/5.txt > evaluator5.log
